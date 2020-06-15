4470d90957da7c027b4fd71a1e492f37a351024f
Anderson Luiz Sartori <anderson.trovata@gmail.com>
2020-06-14 22:17:48 -0300

    #App

---
+++ app/src/main/java/br/com/anderson/caraoucoroa/MainActivity.java
@@ -0,0 +1,34 @@
+package br.com.anderson.caraoucoroa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private Button buttonJogar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonJogar =  findViewById(R.id.buttonJogar);
+        buttonJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent( getApplicationContext(), ResultadoActivity.class);
+
+                //passar dados active
+                int numero = new Random().nextInt(2);
+                intent.putExtra("numero", numero);
+                startActivity(intent);
+            }
+        });
+    }
+}
